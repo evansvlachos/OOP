34ce08ea21394c44bb225c7a3bebd9363a5a4a5d
evansvlachos <49921233+evansvlachos@users.noreply.github.com>
2019-06-14 19:03:51 +0300

    Update and rename insurance.visualbasic to insurance.vb

---
+++ insurance.vb
@@ -0,0 +1,24 @@
+button 1 click
+tameio=înputbox ('Dwse tameio')
+visit=inputbox('Δωσε επισκεψεις')
+poso=inputbox('dose poso')
+sum=0
+sum=0
+sum=0
+sum=0
+Do while visit>=0
+sum=sum+poso
+if tameio="OPAD" then
+sum0=sum0=poso*80/100
+else if tameio="ika" then
+sum=sum+visit>10 then
+p10=p10+1
+endif
+po=po+1
+endif
+loop
+textbox1.text=sum  to string()
+textbox2.text=sumi to string()
+textbox3.text=sumo to string()
+textbox4.text=sumb to string()
+textbox5.text=posos to string()

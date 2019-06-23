461d269b8b53f5b3dd9bef0d35a580a53b30613c
evansvlachos <49921233+evansvlachos@users.noreply.github.com>
2019-06-23 04:57:35 +0300

    Update and rename sqrt to 1equetion.vb

---
+++ 1equetion.vb
@@ -0,0 +1,13 @@
+dim a,x,b on double
+a=double.parse(textbox1.text)
+b=double.parse(textbox2.text)
+if a=0 then
+if b=0 then
+textbox3.text="aoristh"
+else
+textbox4.text="adynath"
+endif
+else 
+x=-c/b cstr
+textbox3.text=x.to string 
+end if

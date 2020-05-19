b2e99aaea4ee360cadacb1b99544395353426723
evansvlachos <49921233+evansvlachos@users.noreply.github.com>
2020-05-19 23:47:49 +0300

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,5 +1,16 @@
 public class MyClass {
   public static void main(String[] args) {
-    System.out.println("δωσε το ποσο ");
+    int GCP= 600;
+    int OLD=601;
+ if (GCP < OLD) {
+  System.out.println("Depresion");
+} 
+else (GCP=OLD){
+  System.out.println("stable");
+}   
+  else (GCP>OLD){
+  System.out.println("growth");
+}
+    // Outputs "Good evening."
   }
 }

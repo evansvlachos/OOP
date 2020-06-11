271f6d65af253df0f1401732cbb6971ba679a7ce
evansvlachos <49921233+evansvlachos@users.noreply.github.com>
2020-06-11 12:32:41 +0300

    Update 1.java

--- 1.java
+++ 1.java
@@ -4,8 +4,26 @@ public class MyClass {
    int GCP= 600;
    int OLD=601;
 
- //FORM     
-JLABEL lblName =new label ("");
+ /*FORM     
+JLABEL lblName =new label ("");*/
+
+import java.util.Scanner;  // Import the Scanner class
+
+class MyClass {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter gcp");
+    int GCP = myObj.nextLine();  // Read user input
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter old");
+    int old = myObj.nextLine();  // Read user input
+    
+    
+    System.out.println("Username is: " + userName);  // Output user input
+  }
+}
+    
+    
   
 //gcp select LOOP 
  if (GCP < OLD) {
